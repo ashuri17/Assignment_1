53ede5f247d6e8b9f3962cb6ef5d76dc67ecb671
ashuri <s10257765@connect.np.edu.sg>
2023-12-01 01:51:15 +0800

    Added a js file

---
+++ script.js
@@ -0,0 +1,8 @@
+function openSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'flex';
+}
+function closeSidebar(){
+    const sidebar = document.querySelector(".sidebar")
+    sidebar.style.display = 'none';
+}
