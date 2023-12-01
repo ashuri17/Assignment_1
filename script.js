5bd291d0ee9439297342228e610ec72ddf6a4a36
ashuri <s10257765@connect.np.edu.sg>
2023-12-01 22:16:12 +0800

    Added dropdown menu for size options

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 function openSidebar(){
-    const sidebar = document.querySelector(".sidebar")
+    const sidebar = document.querySelector(".sidebar")  /*open sidebar when mouseclick on menu button*/
     sidebar.style.display = 'flex';
 }
 function closeSidebar(){
-    const sidebar = document.querySelector(".sidebar")
+    const sidebar = document.querySelector(".sidebar") /*close sidebar when mouseclick on the X button*/
     sidebar.style.display = 'none';
+}
+function toggleDropdown(){
+    var content = document.getElementById("dropdownID"); /*function to show/hide size options*/
+    if (content.style.display === "none" || content.style.display === ""){
+        content.style.display = "block";
+    }
+    else{
+        content.style.display = "none"
+    }
 }
